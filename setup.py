86f8f3540c15141048277fcf20375dde1d7a82dc
Dipesh Acharya <xtranophilist@gmail.com>
2014-09-25 23:31:03 +0545

    bumped version to 1.2.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-froala-editor',
-    version='1.2.1',
+    version='1.2.11',
     author='Dipesh Acharya',
     author_email='xtranophilist@gmail.com',
     packages=['froala_editor'],
