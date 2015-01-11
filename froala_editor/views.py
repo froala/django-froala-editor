60ec199f421d9968f15215e745427a9c8ed45577
Dipesh Acharya <xtranophilist@gmail.com>
2015-01-11 19:17:59 +0545

    used FileSystemStorage

--- froala_editor/views.py
+++ froala_editor/views.py
@@ -1,33 +1,29 @@
 import json
 # from django.http import JsonResponse
+import urlparse
 from django.http import HttpResponse
 from django.conf import settings
 import uuid
 import os
+from django.core.files.storage import FileSystemStorage
 
 
 def image_upload(request):
     if 'file' in request.FILES:
-        file = request.FILES['file']
+        the_file = request.FILES['file']
         allowed_types = ['image/jpeg', 'image/jpg', 'image/pjpeg', 'image/x-png', 'image/png', 'image/gif']
-        if not file.content_type in allowed_types:
-            return HttpResponse(json.dumps({}), mimetype="application/json")
+        if not the_file.content_type in allowed_types:
+            return HttpResponse(json.dumps({}), content_type="application/json")
         # Other data on the request.FILES dictionary:
-        #   filesize = len(file['content'])
-        #   filetype = file['content-type']
+        # filesize = len(file['content'])
+        # filetype = file['content-type']
         media_root = getattr(settings, 'MEDIA_ROOT', '.')
         media_url = getattr(settings, 'MEDIA_URL', '.')
-        upload_to = getattr(settings, 'FROALA_UPLOAD_PATH', 'uploads/froala_editor/images')
+        upload_to = getattr(settings, 'FROALA_UPLOAD_PATH', 'uploads/froala_editor/images/')
         full_upload_path = '%s/%s' % (media_root, upload_to)
-        if not os.path.exists(full_upload_path):
-            os.makedirs(full_upload_path)
-        ext = file.name.split('.')[-1]
-        file_name = "%s.%s" % (uuid.uuid4(), ext)
-        path = '%s/%s' % (full_upload_path, file_name)
-        fd = open(path, 'wb')
-        for chunk in file.chunks():
-            fd.write(chunk)
-        fd.close()
-        link = '%s%s/%s' % (media_url, upload_to, file_name)
+        storage = FileSystemStorage(location=full_upload_path)
+        name = storage.save(None, the_file)
+        storage.base_url = urlparse.urljoin(media_url, upload_to)
+        link = storage.url(name)
         # return JsonResponse({'link': link})
         return HttpResponse(json.dumps({'link': link}), content_type="application/json")
