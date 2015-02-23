339114da435ffeb3b0da3ffdd704851a486dd2dd
Stefan Neculai <stefan.neculai@gmail.com>
2015-02-23 11:32:39 +0200

    Version 1.2.6.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-froala-editor',
-    version='1.2.5',
+    version='1.2.6',
     author='Dipesh Acharya',
     author_email='xtranophilist@gmail.com',
     packages=['froala_editor'],
