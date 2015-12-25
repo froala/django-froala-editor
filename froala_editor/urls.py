805ffdb93fcd98d0d5865f73d19efa760e1b385e
eM <whoknowwhat0623@gmail.com>
2015-12-25 16:43:32 +0900

    Migrate to Django 1.9
    - RemovedInDjango110Warning: django.conf.urls.patterns() is deprecated
      and will be removed in Django 1.10. Update your urlpatterns to be a
      list of django.conf.urls.url() instances instead.
        url(r'^file_upload/$', views.file_upload,
        name='froala_editor_file_upload'),

--- froala_editor/urls.py
+++ froala_editor/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from froala_editor import views
 
-urlpatterns = patterns('',
-                       url(r'^image_upload/$', views.image_upload, name='froala_editor_image_upload'),
-                       url(r'^file_upload/$', views.file_upload, name='froala_editor_file_upload'),
-)
+urlpatterns = [
+    url(r'^image_upload/$', views.image_upload, name='froala_editor_image_upload'),
+    url(r'^file_upload/$', views.file_upload, name='froala_editor_file_upload'),
+]
