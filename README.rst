7b0287e909097e6d3284d79efb139394d8d7bcd1
Stefan Neculai <stefan.neculai@gmail.com>
2016-03-02 21:55:25 +0200

    Update to 2.2.1.

--- README.rst
+++ README.rst
@@ -138,7 +138,7 @@ Froala Editor comes with the plugins: block style, text & background colors, fon
 
 .. code-block:: python
 
-    FROALA_EDITOR_PLUGINS = ('align', 'char_counter', 'code_beautifier' ,'code_view', 'colors', 'emoticons',
+    FROALA_EDITOR_PLUGINS = ('align', 'char_counter', 'code_beautifier' ,'code_view', 'colors', 'draggable', 'emoticons',
             'entities', 'file', 'font_family', 'font_size', 'fullscreen', 'image_manager', 'image', 'inline_style',
             'line_breaker', 'link', 'lists', 'paragraph_format', 'paragraph_style', 'quick_insert', 'quote', 'save', 'table',
             'url', 'video')
--- froala_editor/__init__.py
+++ froala_editor/__init__.py
@@ -1,10 +1,10 @@
 PLUGINS = (
-    'align', 'char_counter', 'code_beautifier', 'code_view', 'colors', 'emoticons', 'entities', 'file', 'font_family',
+    'align', 'char_counter', 'code_beautifier', 'code_view', 'colors', 'draggable', 'emoticons', 'entities', 'file', 'font_family',
     'font_size', 'fullscreen', 'image', 'image_manager', 'inline_style', 'line_breaker', 'link', 'lists',
     'paragraph_format', 'paragraph_style', 'quick_insert', 'quote', 'save', 'table', 'url', 'video'
 )
 
 PLUGINS_WITH_CSS = (
-    'char_counter', 'code_view', 'colors', 'emoticons', 'file', 'fullscreen', 'image', 'image_manager', 'line_breaker',
+    'char_counter', 'code_view', 'colors', 'draggable', 'emoticons', 'file', 'fullscreen', 'image', 'image_manager', 'line_breaker',
     'quick_insert', 'table', 'video',
 )
