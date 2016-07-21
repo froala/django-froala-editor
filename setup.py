43e93075c89d60f357f533b912f87b782a078510
Stefan Neculai <stefan.neculai@gmail.com>
2016-07-21 16:26:55 +0300

    Update to V2.3.4.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-froala-editor',
-    version='2.3.3',
+    version='2.3.4',
     author='Dipesh Acharya',
     author_email='xtranophilist@gmail.com',
     maintainer='Froala Labs',
