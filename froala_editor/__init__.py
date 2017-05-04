38f6b0841701f78418cbb0d3a5235be677b0b3e3
Stefan Neculai <stefan.neculai@gmail.com>
2017-05-04 14:12:12 +0300

    Update to V2.6.0.

--- froala_editor/__init__.py
+++ froala_editor/__init__.py
@@ -1,10 +1,11 @@
 PLUGINS = (
     'align', 'char_counter', 'code_beautifier', 'code_view', 'colors', 'draggable', 'emoticons', 'entities', 'file', 'font_family',
-    'font_size', 'fullscreen', 'help', 'image', 'image_manager', 'inline_style', 'line_breaker', 'link', 'lists',
-    'paragraph_format', 'paragraph_style', 'print', 'quick_insert', 'quote', 'save', 'special_characters', 'table', 'url', 'video',          'word_paste'
+    'font_size', 'fullscreen', 'help', 'image', 'image_aviary', 'image_manager', 'inline_style', 'line_breaker', 'link', 'lists',
+    'paragraph_format', 'paragraph_style', 'print', 'quick_insert', 'quote', 'save', 'special_characters', 'spell_checker',
+    'table', 'url', 'video', 'word_paste'
 )
 
 PLUGINS_WITH_CSS = (
     'char_counter', 'code_view', 'colors', 'draggable', 'emoticons', 'file', 'fullscreen', 'image', 'image_manager', 'line_breaker',
-    'quick_insert', 'table', 'special_characters', 'video', 'help'
+    'quick_insert', 'table', 'special_characters', 'spell_checker', 'video', 'help'
 )
