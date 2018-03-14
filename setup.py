0812a2107c06e574106ea9149bb8dc268eac795e
Dipesh Acharya <xtranophilist@gmail.com>
2018-03-14 19:40:05 +0545

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-froala-editor',
-    version='2.7.6-1',
+    version='2.7.6-2',
     author='Dipesh Acharya',
     author_email='xtranophilist@gmail.com',
     maintainer='Froala Labs',
