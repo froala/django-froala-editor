4dfd615219bf0b5f88a2184f333174137783c18a
Dipesh Acharya <xtranophilist@gmail.com>
2018-05-15 16:59:50 +0545

    Add Python 3 as supported language in setup.py

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
