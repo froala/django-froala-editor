945db767e9a54cd6286ae8c23ae9e369bb45cfe8
Navneet Kumar <navneet.kumar@accoliteindia.com>
2019-07-15 14:54:37 +0530

    Update to 3.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-froala-editor',
-    version='3.0.2',
+    version='3.0.3',
     author='Dipesh Acharya',
     author_email='dipesh@awecode.com',
     maintainer='Froala Labs',
