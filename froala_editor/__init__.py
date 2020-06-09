84e9fbee0e3e4473b6f8eb635549983df923d0dd
geet06 <jasgeet.sekhon@accoliteindia.com>
2020-06-09 21:29:20 +0530

    v3.2.0 dist and added files_manager

--- froala_editor/__init__.py
+++ froala_editor/__init__.py
@@ -1,12 +1,12 @@
 PLUGINS = (
     'align', 'char_counter', 'code_beautifier', 'code_view', 'colors', 'draggable', 'emoticons', 'entities', 'file',
-    'font_family', 'font_size', 'fullscreen', 'help', 'image', 'image_manager', 'inline_style', 'line_breaker', 'link', 'lists',
+    'font_family','files_manager', 'font_size', 'fullscreen', 'help', 'image', 'image_manager', 'inline_style', 'line_breaker', 'link', 'lists',
     'paragraph_format', 'paragraph_style', 'print', 'quick_insert', 'quote', 'save', 'special_characters', 'table', 'url',
     'video', 'word_paste'
 )
 
 PLUGINS_WITH_CSS = (
-    'char_counter', 'code_view', 'colors', 'draggable', 'emoticons', 'file', 'fullscreen', 'help', 'image', 'image_manager',
+    'char_counter', 'code_view', 'colors', 'draggable', 'emoticons', 'file', 'fullscreen','files_manager', 'help', 'image', 'image_manager',
     'line_breaker', 'quick_insert', 'special_characters', 'table', 'video',
 )
 
