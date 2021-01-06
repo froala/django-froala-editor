0ae496e7c3bff1f1ffe4adc8620ed2aea38637c1
Kapil Garg <kapil.garg.techy@gmail.com>
2021-01-06 14:13:39 +0530

    Update to 3.2.5-1 version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-froala-editor',
-    version='3.2.2',
+    version='3.2.5-1',
     author='Dipesh Acharya',
     author_email='dipesh@awecode.com',
     maintainer='Froala Labs',
