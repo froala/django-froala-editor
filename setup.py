c2999bab757c4e86c802a673114c024667f1bcd0
Kapil Garg <kapil.garg.techy@gmail.com>
2021-06-03 12:03:16 +0530

    Updated to v4.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-froala-editor',
-    version='3.2.7',
+    version='4.0.0',
     author='Dipesh Acharya',
     author_email='dipesh@awecode.com',
     maintainer='Froala Labs',
