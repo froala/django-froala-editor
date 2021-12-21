4eafa180e844f8d9e2df3b10ed92debbcd9f91f4
harasunu-narayan <60644555+harasunu-narayan@users.noreply.github.com>
2021-12-21 13:18:34 +0530

    Update to v4.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-froala-editor',
-    version='4.0.7',
+    version='4.0.8',
     author='Dipesh Acharya',
     author_email='dipesh@awecode.com',
     maintainer='Froala Labs',
