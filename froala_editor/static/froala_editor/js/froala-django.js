000adaf635490bc795c16eb53948c6da6e369367
harasunu-narayan <harasunu.narayan@celestialsys.com>
2026-08-19 17:35:21 +0530

    Adding back files got deleted

---
+++ froala_editor/static/froala_editor/js/froala-django.js
@@ -0,0 +1,15 @@
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie != '') {
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i].trim();
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) == (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
